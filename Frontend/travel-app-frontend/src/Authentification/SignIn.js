0a17542cf17242376fe3bca86270b88611ff5ea4
Cezar <alfacez2003@gmail.com>
2024-10-31 15:20:01 +0200

    0.12

---
+++ Frontend/travel-app-frontend/src/Authentification/SignIn.js
@@ -0,0 +1,43 @@
+// src/components/SignIn.js
+import React, { useState } from 'react';
+import '../Styles/SignIn.css'
+
+function SignIn() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle Sign In Logic
+    console.log('SignIn:', { username, password });
+  };
+
+  return (
+    <div>
+      <h2>Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
